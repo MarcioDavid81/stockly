71b7846b347c5d25abc645ccee150ccd61ba1750
marciodavid <marciodavid81@gmail.com>
2025-01-08 22:33:38 -0300

    feat: add initial edit product dropdown-menu

--- app/products/_components/table-colums.tsx
+++ app/products/_components/table-colums.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { Badge } from "@/app/_components/ui/badge";
+import { Button } from "@/app/_components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/app/_components/ui/dropdown-menu";
 import { Product } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { CircleIcon } from "lucide-react";
+import { CircleIcon, ClipboardCopyIcon, EditIcon, MoreHorizontalIcon, Trash2Icon } from "lucide-react";
 
 const getStatusLabel = (status: string) => {
   if (status === "IN_STOCK") {
@@ -33,12 +42,47 @@ export const productTableColumns: ColumnDef<Product>[] = [
       const label = getStatusLabel(product.status);
       return (
         <Badge
-        className="gap-2"
-        variant={label === "Em Estoque" ? "default" : "outline"}
+          className="gap-2"
+          variant={label === "Em Estoque" ? "default" : "outline"}
         >
-        <CircleIcon size={14} className={`${label === "Em Estoque" ? "fill-primary-foreground" : "fill-destructive-foreground"}`} />
+          <CircleIcon
+            size={14}
+            className={`${label === "Em Estoque" ? "fill-primary-foreground" : "fill-destructive-foreground"}`}
+          />
           {label}
         </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "actions",
+    header: "Ações",
+    cell: (row) => {
+      const product = row.row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="sm">
+              <MoreHorizontalIcon size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Ações em Produtos</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="gap-1.5" onClick={() => navigator.clipboard.writeText(product.id)}>
+              <ClipboardCopyIcon size={16} />
+              Copiar ID
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-1.5">
+              <EditIcon size={16} />
+              Editar Produto
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-1.5">
+              <Trash2Icon size={16} />
+              Excluir Produto
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       );
     },
   },
