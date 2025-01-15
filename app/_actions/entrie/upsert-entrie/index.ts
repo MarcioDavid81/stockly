cb92a1ce4af3615b06f2a0dacb54654e98c504bb
marciodavid <marciodavid81@gmail.com>
2025-01-14 22:39:25 -0300

    feat: ensure exit and entrie creation done with a transaction

--- app/_actions/entrie/upsert-entrie/index.ts
+++ app/_actions/entrie/upsert-entrie/index.ts
@@ -6,42 +6,44 @@ import { db } from "@/app/_lib/prisma";
 
 export const createEntrie = async (data: UpsertEntrieSchema) => {
   upsertEntrieSchema.parse(data);
-  const entrie = await db.entrie.create({
-    data: {
-        date: new Date(),
+  await db.$transaction(async (trx) => {
+    const entrie = await trx.entrie.create({
+      data: {
+          date: new Date(),
+      }
+    })
+    for (const product of data.products) {
+  
+      /*Verifica se o produto existe */
+      const productFromDb = await trx.product.findUnique({
+        where: {
+          id: product.id,
+        },
+      });
+      if (!productFromDb) {
+        throw new Error("Product not found");
+      }
+  
+      /*Cria a entrada do produto */
+      await trx.entrieProduct.create({
+        data: {
+          entrieId: entrie.id,
+          productId: product.id,
+          quantity: product.quantity,
+          unitPrice: productFromDb.price,
+        },
+      });
+      await trx.product.update({
+        where: {
+          id: product.id,
+        },
+        data: {
+          stock: {
+            increment: product.quantity,
+          },
+        },
+      });
     }
   })
-  for (const product of data.products) {
-
-    /*Verifica se o produto existe */
-    const productFromDb = await db.product.findUnique({
-      where: {
-        id: product.id,
-      },
-    });
-    if (!productFromDb) {
-      throw new Error("Product not found");
-    }
-
-    /*Cria a entrada do produto */
-    await db.entrieProduct.create({
-      data: {
-        entrieId: entrie.id,
-        productId: product.id,
-        quantity: product.quantity,
-        unitPrice: productFromDb.price,
-      },
-    });
-    await db.product.update({
-      where: {
-        id: product.id,
-      },
-      data: {
-        stock: {
-          increment: product.quantity,
-        },
-      },
-    });
-  }
   revalidatePath("/products");
 };
